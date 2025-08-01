4bd6c0900c4f1d99aceb990b1ab99ad9a6666c0d
Ashmin Bhujel <ashminbhujel01@gmail.com>
2025-08-01 21:49:19 +0545

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Learn Tanstack Query
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="container mx-auto p-4">
+      <h1 className="text-4xl">Learn Tanstack Query</h1>
+    </main>
+  );
+}
